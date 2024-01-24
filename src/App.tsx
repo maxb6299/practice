f6451629b7f30052d0581bf5c89add9baa2526cd
Max <126419161+maxb6299@users.noreply.github.com>
2024-01-24 12:17:05 -0600

    Implement alert component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["New York", "Miami", "Chicago", "Los Angeles", "Houston"];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
+  // let items = ["New York", "Miami", "Chicago", "Los Angeles", "Houston"];
+  // const handleSelectItem = (item: string) => {
+  //   console.log(item);
+  // };
 
   return (
     <>
-      <ListGroup
+      <Alert>
+        <h1>Alert</h1>
+      </Alert>
+      {/* <ListGroup
         items={items}
         heading="Cities"
         onSelectItem={handleSelectItem}
-      />
+      /> */}
     </>
   );
 }
