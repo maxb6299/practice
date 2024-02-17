118811a98236c5d524b299d99b857280da71a778
Max <126419161+maxb6299@users.noreply.github.com>
2024-02-17 12:20:42 -0500

    Make footer

---
+++ react-beginner-practice-2/src/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return(
+        <>
+            <footer>
+                <div>&copy; {new Date().getFullYear()} My Website Name</div>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
