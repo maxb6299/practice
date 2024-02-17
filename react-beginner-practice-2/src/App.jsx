cfde28896c200034e9ffc787c6eb880ed0d4614d
Max <126419161+maxb6299@users.noreply.github.com>
2024-02-17 12:30:27 -0500

    Make food component

--- react-beginner-practice-2/src/App.jsx
+++ react-beginner-practice-2/src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './Header'
+import Food from './Food'
 import Footer from './Footer'
 
 function App() {
   return (
     <>
       <Header></Header>
+      <Food></Food>
       <Footer></Footer>
     </>
   )
---
+++ react-beginner-practice-2/src/Food.jsx
@@ -0,0 +1,16 @@
+function Food() {
+    const food1 = 'Pasta'
+    const food2 = 'Sushi'
+    const food3 = 'Curry'
+    return (
+        <>
+            <ul>
+                <li>{food1}</li>
+                <li>{food2}</li>
+                <li>{food3}</li>
+            </ul>
+        </>
+    )
+}
+
+export default Food
