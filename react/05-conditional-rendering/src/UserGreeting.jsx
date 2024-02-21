bedda9e4f760902565d426b24a3b7f21891288b7
Max <126419161+maxb6299@users.noreply.github.com>
2024-02-21 12:50:27 -0500

    Reformat component code structure for readability

--- react/05-conditional-rendering/src/UserGreeting.jsx
+++ react/05-conditional-rendering/src/UserGreeting.jsx
@@ -1,7 +1,11 @@
 function UserGreeting(props) {
+    const welcomeMessage = <h2 className='welcome-message'>Welcome {props.username}</h2>
+
+    const loginMessage = <h2 className='login-message'>Please log in to continue</h2> 
+    
     return(
-        props.isLoggedIn ? <h2 className='welcome-message'>Welcome {props.username}</h2>
-                         : <h2 className='login-message'>Please log in to continue</h2> 
+        props.isLoggedIn ? welcomeMessage
+                         : loginMessage
     )
 }
 
