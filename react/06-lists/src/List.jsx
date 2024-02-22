f146b92cd913b81ac95a7e6a4032d2765e223831
Max <126419161+maxb6299@users.noreply.github.com>
2024-02-22 11:24:28 -0500

    Filter high calorie fruit

--- react/06-lists/src/List.jsx
+++ react/06-lists/src/List.jsx
@@ -1,5 +1,5 @@
 function List() {
-    const fruitList = [
+    let fruitList = [
         {id: 1, name: 'apple', calories: 90},
         {id: 2, name: 'banana', calories: 40},
         {id: 3, name: 'grapes', calories: 210},
@@ -9,6 +9,8 @@ function List() {
 
     fruitList.sort((a,b) => a.calories - b.calories);
 
+    fruitList = fruitList.filter(fruit => fruit.calories < 100);
+    
     const fruitListDiv = fruitList.map(fruit => 
         <li key={fruit.id}>{fruit.name}: {fruit.calories} kCal</li>
     );
