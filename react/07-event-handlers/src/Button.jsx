7ac2f68922019af27a73dad9abf94b232b4a4401
Max <126419161+maxb6299@users.noreply.github.com>
2024-02-22 11:59:42 -0500

    Implement click handler function

--- react/07-event-handlers/src/Button.jsx
+++ react/07-event-handlers/src/Button.jsx
@@ -1,7 +1,15 @@
 function Button() {
+    let count = 0;
+    let name = 'User';
+    
+    const handleClick = (username) => {
+        count++;
+        console.log(`${username} has clicked me ${count} times`);
+    }
+    
     return(
         <>
-            <button>Click me</button>
+            <button onClick={() => handleClick(name)}>Click me</button>
         </>
     )
 }
