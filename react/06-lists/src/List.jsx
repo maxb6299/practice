953edf15d2d4da342121efa1958a2d6d89c3f650
Max <126419161+maxb6299@users.noreply.github.com>
2024-02-22 11:15:36 -0500

    Create sorted list of fruit

--- react/06-lists/src/List.jsx
+++ react/06-lists/src/List.jsx
@@ -1,7 +1,21 @@
 function List() {
+    const fruitList = [
+        {id: 1, name: 'apple', calories: 90},
+        {id: 2, name: 'banana', calories: 40},
+        {id: 3, name: 'grapes', calories: 210},
+        {id: 4, name: 'strawberries', calories: 70},
+        {id: 5, name: 'orange', calories: 70},
+    ];
+
+    fruitList.sort((a,b) => a.calories - b.calories);
+
+    const fruitListDiv = fruitList.map(fruit => 
+        <li key={fruit.id}>{fruit.name}: {fruit.calories} kCal</li>
+    );
+    
     return (
         <>
-            
+            <ol>{fruitListDiv}</ol>
         </>
     )
 }
