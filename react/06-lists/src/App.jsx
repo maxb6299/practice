25b433da00d6d1a5994a0d28d37eff5a145e7e3b
Max <126419161+maxb6299@users.noreply.github.com>
2024-02-22 11:32:30 -0500

    Make list prop-friendly Make vegetable list

--- react/06-lists/src/App.jsx
+++ react/06-lists/src/App.jsx
@@ -1,9 +1,27 @@
 import List from './List'
 
 function App() {
+  const fruitList = [
+        {id: 1, name: 'apple', calories: 90},
+        {id: 2, name: 'banana', calories: 40},
+        {id: 3, name: 'grapes', calories: 210},
+        {id: 4, name: 'strawberries', calories: 70},
+        {id: 5, name: 'orange', calories: 70}
+      ];
+
+  const vegetableList = [
+    {id: 1, name: 'broccoli', calories: 30},
+    {id: 2, name: 'carrots', calories: 30},
+    {id: 3, name: 'cauliflower', calories: 50},
+    {id: 4, name: 'peas', calories: 70},
+    {id: 5, name: 'corn', calories: 10}
+  ]
+    
+  
   return (
     <>
-      <List />
+      <List array={fruitList} category='Fruits' />
+      <List array={vegetableList} category='Vegetables' />
     </>
   )
 }
--- react/06-lists/src/List.jsx
+++ react/06-lists/src/List.jsx
@@ -1,21 +1,14 @@
-function List() {
-    const fruitList = [
-        {id: 1, name: 'apple', calories: 90},
-        {id: 2, name: 'banana', calories: 40},
-        {id: 3, name: 'grapes', calories: 210},
-        {id: 4, name: 'strawberries', calories: 70},
-        {id: 5, name: 'orange', calories: 70},
-    ];
+function List(props) {
+    props.array.sort((a,b) => a.calories - b.calories);
 
-    fruitList.sort((a,b) => a.calories - b.calories);
-
-    const fruitListDiv = fruitList.map(fruit => 
-        <li key={fruit.id}>{fruit.name}: {fruit.calories} kCal</li>
+    const listDiv = props.array.map(item => 
+        <li key={item.id}>{item.name}: {item.calories} kCal</li>
     );
     
     return (
         <>
-            <ol>{fruitListDiv}</ol>
+            <h2>{props.category}</h2>
+            <ol>{listDiv}</ol>
         </>
     )
 }
